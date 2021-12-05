00340bbf89f298b81495c900a1b34d800a18be2f
Dajovih <80296654+Dajovih@users.noreply.github.com>
2021-12-05 13:30:11 -0500

    Modificación de las clases Administracion y Visitante
    Se implementan los atributos y métodos respectivos de cada clase.

--- src/gestorAplicacion/gestionZoologico/Administracion.java
+++ src/gestorAplicacion/gestionZoologico/Administracion.java
@@ -1,5 +1,105 @@
 package gestorAplicacion.gestionZoologico;
 
-public class Administracion {
+import java.util.*;
+import gestorAplicacion.animalesZoologico.*;
 
-}
+public class Administracion {
+	private double caja;
+	static private List<Animal> animales=new ArrayList<Animal>();
+	static private List<Empleado> empleados=new ArrayList<Empleado>();
+	static private List<Visitante> visitantes=new ArrayList<Visitante>();
+	static private List<Habitat> habitats= new ArrayList<Habitat>();
+	
+	public Administracion(int caja) {
+		this.caja=caja;}
+	
+	public void pagoNomina() {
+		double pago=0;
+		double ganancias=calculoGanancias();
+		caja=caja+ganancias;
+		for (Empleado empleado:empleados) {
+			pago=pago+empleado.getSueldo();
+		}
+		caja=caja-pago;
+		if (caja<0) {
+			
+		}
+		
+	}
+	
+	public void trasladarAnimal(Animal animal) {
+		
+	}
+	
+	public void adquirirAnimal(Animal animal) {
+		
+	}
+	
+	public double calculoGanancias() {
+		double ganancias=0;
+		for (Visitante visitante:visitantes) {
+			ganancias=ganancias+visitante.getPrecioBoleta();}
+		return ganancias;}
+	
+	public void contratarEmpleado() {
+		
+	}
+	
+	public void despedirEmpleado(Empleado empleado) {
+		removeEmpleados(empleado);
+		
+	}
+	
+	public static void addAnimales(Animal nuevo) {
+		animales.add(nuevo);}
+	
+	public static void addEmpleados(Empleado nuevo) {
+		empleados.add(nuevo);}
+	
+	public static void addVisitantes(Visitante nuevo) {
+		visitantes.add(nuevo);}
+	
+	public static void addHabitats(Habitat nuevo) {
+		habitats.add(nuevo);}
+	
+	public static void removeAnimales(Animal eliminar) {
+		animales.remove(animales.indexOf(eliminar));}
+	
+	public static void removeEmpleados(Empleado eliminar) {
+		empleados.remove(empleados.indexOf(eliminar));}
+	
+	public static void removeVisitantes(Visitante eliminar) {
+		visitantes.remove(visitantes.indexOf(eliminar));}
+	
+	public static void removeHabitats(Habitat eliminar) {
+		habitats.remove(habitats.indexOf(eliminar));}
+	
+	public double getCaja() {
+		return caja;}
+	
+	public static List<Animal> getAnimales() {
+		return animales;}
+	
+	public static List<Empleado> getEmpleados() {
+		return empleados;}
+	
+	public static List<Visitante> getVisitantes() {
+		return visitantes;}
+	
+	public static List<Habitat> getHabitats() {
+		return habitats;}
+	
+	public void setCaja(double nuevo) {
+		caja=nuevo;}
+	
+	public static void setAnimales(List<Animal> nuevo) {
+		animales=nuevo;}
+	
+	public static void setEmpleados(List<Empleado> nuevo) {
+		empleados=nuevo;}
+	
+	public static void setVisitantes(List<Visitante> nuevo) {
+		visitantes=nuevo;}
+	
+	public static void setHabitats(List<Habitat> nuevo) {
+		habitats=nuevo;}}
--- src/gestorAplicacion/gestionZoologico/Visitante.java
+++ src/gestorAplicacion/gestionZoologico/Visitante.java
@@ -1,5 +1,71 @@
 package gestorAplicacion.gestionZoologico;
 
 public class Visitante {
+	private int identificacion;
+	private String nombre;
+	private int estrato;
+	private int edad;
+	private double precioBoleta;
+	private static int totalVisitantes;
 
-}
+	public Visitante(int identificacion, String nombre, int estrato, int edad) {
+		this.identificacion=identificacion;
+		this.nombre=nombre;
+		this.estrato=estrato;
+		this.edad=edad;
+		totalVisitantes++;
+		Administracion.addVisitantes(this);}
+	
+	public void salidaVisitante(Visitante visitante) {
+		totalVisitantes--;
+		Administracion.removeVisitantes(visitante);
+		}
+	
+	public void calcularPrecioBoleta() {
+		if (edad<15) {
+			precioBoleta=10000;
+			if (estrato<=3) {
+				precioBoleta=precioBoleta*0.7;
+			}
+		}
+		else {
+			precioBoleta=20000;
+			if (estrato<=3) {
+				precioBoleta=precioBoleta*0.8;}}}	
+	
+	public int getIdentificacion() {
+		return identificacion;}
+	
+	public String getNombre() {
+		return nombre;}
+	
+	public int getEstrato() {
+		return estrato;}
+	
+	public int getEdad() {
+		return edad;}
+	
+	public double getPrecioBoleta() {
+		return precioBoleta;}
+	
+	public static int getTotalVisitantes() {
+		return totalVisitantes;}
+	
+	public void setIdentificacion(int nuevo) {
+		identificacion=nuevo;}
+	
+	public void setNombre(String nuevo) {
+		nombre=nuevo;}
+	
+	public void setEstrato(int nuevo) {
+		estrato=nuevo;}
+	
+	public void setEdad(int nuevo) {
+		edad=nuevo;}
+	
+	public void setPrecioBoleta(double nuevo) {
+		precioBoleta=nuevo;}
+	
+	public static void setTotalVisitantes(int nuevo) {
+		totalVisitantes=nuevo;}}
+
