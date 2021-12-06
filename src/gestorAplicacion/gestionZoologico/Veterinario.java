7d95f9c29bcd2cda7a3f5f18b528408c045bf739
jjmonsalvem <jjmonsalvem@unal.edu.co>
2021-12-05 21:41:19 -0500

    Clase Especie #0
    Se sube la primera versión de la clase “Especie”

--- src/gestorAplicacion/gestionZoologico/Veterinario.java
+++ src/gestorAplicacion/gestionZoologico/Veterinario.java
@@ -1,4 +1,47 @@
 package gestorAplicacion.gestionZoologico;
 
+import gestorAplicacion.animalesZoologico.Animal;
+import gestorAplicacion.animalesZoologico.Especie;
+//import java.util.*;
+
 public class Veterinario extends Empleado{
+
+    private Especie especialidad;
+
+
+    public Veterinario(int identificacion, String nombre, int sueldo, Especie especialidad) {
+        super(identificacion, nombre, sueldo);
+        this.especialidad = especialidad;
+    }
+
+    // TODO: Terminar metodo.
+    public boolean revisarAnimal(Animal animal) {
+        return false;
+    }
+
+    public void curarAnimal(Animal animal) {
+        animal.setEstadoSalud(true);
+    }
+
+    public Especie getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(Especie especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    @Override
+    public String toString() {
+        return ("Tipo de empleado: VETERINARIO" + 
+				"\nIdentificación: " + this.getIdentificacion() + 
+				"\nNombre: " + this.getNombre() + 
+				"\nSueldo: " + this.getSueldo() + 
+				"\nEspecie asignada: " + this.getEspecialidad());
+    }
+
+
+    
+
+    
 }
