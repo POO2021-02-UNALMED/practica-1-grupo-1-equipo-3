aa11b8edc0a24ce97724254ac5908df2b7597aff
David M García <davgarciava@unal.edu.co>
2022-01-30 09:54:53 -0500

    Paquetes viejos eliminados

--- Python/src/gestorAplicacion/gestionZoologico/entidad.py
+++
@@ -1,8 +0,0 @@
-# INTERFAZ CREADA POR DAVID MATEO GARCÍA
-
-# Esta interfaz es utilidad para forzar la definición del método info() en las clases que la implemente.
-# Las clases que implementan esta interfaz son: Cuidador, Veterinario, Visitante, Animal, Especie y Habitat. */
-
-class Entidad:
-    def info():
-        pass
