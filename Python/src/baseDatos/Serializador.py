77c9c0ffff226b7a25c94345cb636ac5e1ee707d
Dajovih <80296654+Dajovih@users.noreply.github.com>
2022-02-03 12:49:49 -0500

    Serialización - cambios
    Intentando serializar pero genera error  de importación. Buscando solución

--- Python/src/baseDatos/Serializador.py
+++ Python/src/baseDatos/Serializador.py
@@ -1,35 +1,43 @@
 from gestorAplicacion.administracion import Administracion
-from gestorAplicacion.animal import Animal
-from gestorAplicacion.cuidador import Cuidador
-from gestorAplicacion.habitat import Habitat
-from gestorAplicacion.veterinario import Veterinario
-from gestorAplicacion.visitante import Visitante
+
 import pickle
 
+def serializar():
+    serializarAdmin()
+    serializarAnimales()
+    serializarCuidadores()
+    serializarVeterinarios()
+    serializarHabitats()
+    serializarVisitantes()
+    
+def serializarAdmin():
+    admin=Administracion(0)
+    fichero_admin=open("admin","wb")
+    pickle.dump(admin,fichero_admin)
+    fichero_admin.close()
 
-class Serializador():
-    def serializar():
-        """admin=open("administracion","wb")
-        pickle.dump(Administracion,admin)
-        admin.close()"""
+def serializarAnimales():
+    fichero_animales=open("animales","wb")
+    pickle.dump(Administracion.getAnimales(),fichero_animales)
+    fichero_animales.close()
 
-        fichero_animales=open("animales","wb")
-        pickle.dump(Administracion.getAnimales,fichero_animales)
-        fichero_animales.close()
+def serializarCuidadores():
+    fichero_cuidadores=open("cuidadores","wb")
+    pickle.dump(Administracion.getCuidadores(),fichero_cuidadores)
+    fichero_cuidadores.close()
 
-        fichero_cuidadores=open("cuidadores","wb")
-        pickle.dump(Administracion.getCuidadores,fichero_cuidadores)
-        fichero_cuidadores.close()
+def serializarVeterinarios():
+    fichero_veterinarios=open("veterinarios","wb")
+    pickle.dump(Administracion.getVeterinarios(),fichero_veterinarios)
+    fichero_veterinarios.close()
 
-        fichero_habitats=open("habitats","wb")
-        pickle.dump(Administracion.getHabitats,fichero_habitats)
-        fichero_habitats.close()
+def serializarHabitats():
+    fichero_habitats=open("habitats","wb")
+    pickle.dump(Administracion.getHabitats(),fichero_habitats)
+    fichero_habitats.close()
 
-        fichero_veterinarios=open("veterinarios","wb")
-        pickle.dump(Administracion.getVeterinarios,fichero_veterinarios)
-        fichero_veterinarios.close()
+def serializarVisitantes():
+    fichero_visitantes=open("visitantes","wb")
+    pickle.dump(Administracion.getVisitantes(),fichero_visitantes)
+    fichero_visitantes.close()
 
-        fichero_visitantes=open("visitantes","wb")
-        pickle.dump(Administracion.getVisitantes,fichero_visitantes)
-        fichero_visitantes.close()
-        
--- Python/src/prueba.py
+++ Python/src/prueba.py
@@ -7,21 +7,61 @@ from gestorAplicacion.especie import Especie
 from gestorAplicacion.habitat import Habitat
 from gestorAplicacion.veterinario import Veterinario
 from gestorAplicacion.visitante import Visitante
+from seria import *
 
 if __name__=="__main__":
-    ad=Administracion(0)
+    admin = Administracion()
+    a = Habitat("H1", "Pradera", 2)
+    b = Habitat("H2", "Jungla", 1)
+    c= Habitat("H3", "Pantano", 3)
+    d= Habitat("H5", "Rio", 3)
+    e= Habitat("H6","Pantano",2)
+    a1 = Animal(Especie.REPTIL, c, "Macho", 5, 70.0)
+    a2 = Animal(Especie.REPTIL, c, "Hembra", 4, 65.0)
+    a2.setAlimentado(False)
+    a2.setEstadoAnimo(False)
+    a3 = Animal(Especie.ANFIBIO,e,"Macho",2,20.0)
+    a4 = Animal(Especie.ANFIBIO,e,"Hembra",2,15.0)
+    a3.setEstadoAnimo(False)
+    a4.setEstadoAnimo(False)
+    a3.setAlimentado(False)
+    a4.setEstadoSalud(False)
+    a5 = Animal(Especie.PEZ,d,"Macho",2,20.0)
+    a6 = Animal(Especie.PEZ,d,"Macho",1,15.0)
+    a7 = Animal(Especie.PEZ,d,"Hembra",1,20.0)
+    d.setLimpio(False)
+    a5.setEstadoAnimo(False)
+    a6.setEstadoAnimo(False)
+    a7.setEstadoAnimo(False)
+    a7.setAlimentado(False)
+    a5.setAlimentado(False)
+    a5.setEstadoSalud(False)
+    a6.setEstadoSalud(False)
+    a8 = Animal(Especie.ANFIBIO,b,"Hembra",1,20.0)
+    c1 = Cuidador("Jorge", 7000, Especie.MAMIFERO)
+    c2 = Cuidador("Johanna", 1000, Especie.AVE)
+    c3 = Cuidador("Camila", 100000, Especie.REPTIL)
+    c4 = Cuidador("David", 10000, Especie.PEZ)
+    c5 = Cuidador("Juan", 5000, Especie.ANFIBIO)
+    v1 = Veterinario("Elva",10000,Especie.ANFIBIO)
+    v2 = Veterinario("Francisco",12000,Especie.PEZ)
+    vi1 = Visitante("Jose",3,15)
+    vi2 = Visitante("Diego",5,30)
+    vi3 = Visitante("Valeria",6,30)
+
+    serializar()
+    
+    
+    
+    """ad=Administracion(0)
     cu1=Cuidador("Amy",3000000,Especie.ANFIBIO)
     cu2=Cuidador("Paulina",3000000,Especie.AVE)
     ve1=Veterinario("Vale",232,Especie.PEZ)
     ve2=Veterinario("Jil",2323,Especie.MAMIFERO)
     ve3=Veterinario("Juan",3,Especie.ANFIBIO)
     h=Habitat("Juan","Jil",23)
-    h2=Habitat("Jil","asl",11)
-    ad.despedirCuidador(1)
-    ad.despedirVeterinario(2)
-    for c in Administracion.getVeterinarios():
-        print(c.info())
-    for c in Administracion.getCuidadores():
-        print(c.info())
-
+    an1=Animal(Especie.ANFIBIO,h,"Macho",34,2)
+    an1.setAlimentado(False)
+    an1.setEstadoAnimo(False)
+    cu1.revisarAnimal"""
     
