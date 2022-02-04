c8027a438e0875a6bb4bbe156bda30aeba1571e8
Dajovih <80296654+Dajovih@users.noreply.github.com>
2022-02-04 06:19:34 -0500

    Ola

--- Python/src/funcionalidadGestion.py
+++ Python/src/funcionalidadGestion.py
@@ -21,18 +21,21 @@ class Gestion(Frame):
 
     def __init__(self):
         super().__init__()
+        #Creación del label del titulo de la funcionalidad
         nombre = Label(master=self, text="Gestión administrativa", font="Helvetica 11 bold")
         info = """Nos encargaremos de pagarle a cada uno de nuestros empleados, su respectivo sueldo. Para ello 
 calcularemos las ganancias que hemos generado el día de hoy, y se lo sumaremos al presupuesto 
 que posee el zoológico en el banco. Usa el botón pagar para hacer todo esto.
 
 En caso de que no tengamos dinero suficiente para pagarle a los empleados, será necesario 
-que despida a uno de estos para así recuperarnos económicamente, y para ello se habilitará 
-el botón despedir."""
+que despida a uno de estos para así recuperarnos económicamente. Para ello dirigase
+al menu de procesos y consultas, luego en otras funcionalidades, despedir empleado."""
+        #Creación del label de descripción de la funcionalidad
         descripcion = Label(master=self, text=info, font="Helvetica 10")
         nombre.pack(fill=BOTH, padx=5, pady=5)
         descripcion.pack(fill=BOTH, padx=5, pady=5)
 
+        #Creación de botón 'Pagar a empleados' que ejecutará toda la funcionalidad
         botones = Frame(master=self)
         pagar = Button(master=botones, text="Pagar a empleados", font="Helvetica 11 bold", 
                          bg="grey", fg="white", borderwidth=3, relief="raised",
@@ -43,22 +46,25 @@ el botón despedir."""
 
     """@staticmethod"""
     def pagar(self):
+        #Administracion tiene un atributo ganancias, el cual indica si ya se ha pagado a la nómina. 
         if Administracion.isGanancias()==True:
+            #En caso de ser True, mostrará el mensaje de que ya se le pago a los empleados
             mensaje=("Ya hemos pagado a todos los empleados por el día de hoy.\n"+
-            "\nVuelva a ejecutar esta función el día de mañana cuando hayamos recibido más visitantes.")
+            "\nVuelva a ejecutar esta función el día de mañana, cuando hayamos recibido más visitantes.")
             messagebox.showerror(title="Error",message=mensaje)
         else:
+            #Muestra datos de ingresos generados, números de visitantes, número de ganancias, etc...
             mensaje=("Hemos recibido a "+str(len(Administracion.getVisitantes()))+" visitantes el día de hoy y estos "+
             "han generado ganancias por un valor de "+str(Gestion.sumaBoletas())+"$."+
-            " En el banco habían "+str(Administracion.getCaja())+"$ guardados. Ahora cuenta con "+str(Administracion.getCaja()+Gestion.sumaBoletas())+"$."
-            "\n\nContamos con "+str(Gestion.empleados())+" empleados en la nómina. El monto total a pagar por la suma de sueldos es de"+
+            " En el banco habían "+str(Administracion.getCaja())+"$ y ahora hay "+str(Administracion.getCaja()+Gestion.sumaBoletas())+"$."
+            "\n\nContamos con "+str(Gestion.empleados())+" empleados en la nómina. El monto total a pagar por la suma de sueldos es de "+
             Gestion.montoPagar()+" $."+"\n\nProcederemos con el pago.")
             messagebox.showinfo(title="Información",message=mensaje)
             Administracion.calculoGanancias()
             Administracion.pagoNomina()
             if (Administracion.getCaja()<0):
                 mensaje=("Le informamos que no ha tenido dinero suficiente para pagar,"+" y hemos tenido que realizar un prestamo por un valor de "+str(Administracion.getCaja()*-1)+"$."+
-                "\n\nEs necesario que despida a uno de nuestros empelados. Para ello se en el menú de procesos y consultas, dirijase a otrasfuncionalidades y luego en la opción de despedir empelado. ")
+                "\n\nEs necesario que despida a uno de nuestros empleados. Para ello en el menú de procesos y consultas, dirijase a otras funcionalidades y luego en la opción de despedir empelado. ")
                 messagebox.showwarning(title="Dinero insuficiente",message=mensaje)
                 
             else:
